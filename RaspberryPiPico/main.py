fea8dc08c52cb4d9e2bd8417e82d6e2e8ff7ec81
Eliseous-freelance <96338130+Eliseous-freelance@users.noreply.github.com>
2022-09-08 19:47:17 +0100

    Update main.py

--- RaspberryPiPico/main.py
+++ RaspberryPiPico/main.py
@@ -1,20 +1,42 @@
+#board packages
 import time
 import board
 import adafruit_dht
 import analogio
 
+#temperature and humdity
 dht = adafruit_dht.DHT11(board.GP27)
 adc = analogio.AnalogIn(board.GP26)
+#light intensity
 def getIntense(sensor):
     return (sensor.value / 65535 * adc.reference_voltage)/3.7
-#print(dht)
-while True:
-    print("Temperature",dht.temperature,"C")
-    print("Humidity", dht.humidity,"%")
-    print("Light intesnity",getIntense(adc)*100,"%")
-    time.sleep(1)
-    if dht.temperature>25:
-        print("Damnnnn you hot dummy thiccc")
+
+
+from github import Github
+import pandas as pd
+g = Github("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")
+
+repo = g.get_user().get_repo('Terrarium')
+
+print(pd.read_csv(repo.get_contents('data/database.csv').download_url))
+
+
+#while True:
+#  
+#    temperature = dht.temperature
+#    humdity = dht.temperature
+#    light_intensity = getIntense(adc)*100
+#    
+#    
+#    time.sleep(1)
+
+    #print("Temperature",temperature,"C")
+    #print("Humidity", humdity,"%")
+    #print("Light intesnity",light_intensity,"%")
+    #if temperature>25:
+    #    print("Damnnnn you hot dummy thiccc")
         
-    
+        
+
+
 
