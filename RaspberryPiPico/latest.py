2fb7a6cc6301f41c745e06ceeda8cf06054b111a
Eliseous-freelance <96338130+Eliseous-freelance@users.noreply.github.com>
2022-09-29 16:17:56 +0100

    Update latest.py

--- RaspberryPiPico/latest.py
+++ RaspberryPiPico/latest.py
@@ -1,57 +1,109 @@
 #board packages
 import time
 import board
-import lib.adafruit_dht as adafruit_dht
+import lib.adafruit_dht as adafruit_dht #adafruit library for dht11 sensor
 import analogio
 
-import os
-import json
+import json 
+import os #paths and files package
 import io
 
-#import sys
+#storage packages
+import busio
+import sdcardio
+import storage
 
-#temperature and humdity
-dht = adafruit_dht.DHT11(board.GP27)
-adc = analogio.AnalogIn(board.GP26)
+"""
 
-#sys.path.append('/')
-#light intensity
-def getIntense(sensor):
-   return (sensor.value / 65535 * adc.reference_voltage)/3.7
+"""
+class Ecosystem:
+    """
+    class constructor
+    """
+    def __init__(self):
+        #temperature and humdity
+        self.dht = adafruit_dht.DHT11(board.GP22) #original 27
+        self.adc = analogio.AnalogIn(board.GP26) #original 26
+        
+        self.temperature, self.humidity, self.light_intensity = self.get_paramaters()
+    
+    """
+    reads from sensors
+    return: temperature
+    return: humidity
+    return: light_intensity
+    """
+    def get_paramaters(self):
+        temperature = dht.temperature
+        humidity = dht.temperature
+        light_intensity = self.get_intensity(adc)*100
+        return temperature, humditiy, light_intensity
 
+    """
+    light intensity getter
+    return: light intensity
+    """
+    def get_intensity(self, sensor):
+       return (sensor.value / 65535 * self.adc.reference_voltage)/3.7
 
-print(os.uname())
-print(os.listdir('/'))
+"""
+reads existing data in data.json file
+return: existing_data data that has been previously stored
+"""
+def get_existing_data():
+    try:
+        with open('/sd/data.json') as f:
+                existing_data = json.load(f)
+    except Exception as e:
+        print('error loading existing data', e)
+    return existing_data
+
+time.sleep(1)
+#load board and pins
+spi = busio.SPI(board.GP10, MOSI=board.GP11, MISO=board.GP12)
+cs = board.GP15
+sd = sdcardio.SDCard(spi, cs)
+
+vfs = storage.VfsFat(sd)
+storage.mount(vfs, '/sd')
 
 while True:
-    temperature = dht.temperature
-    humidity = dht.temperature
-    light_intensity = getIntense(adc)*100
-    date_time = time.localtime()
-        
-    time.sleep(1)
-
-    
-    data = {"date":f"{date_time.tm_year}-{date_time.tm_mon}-{date_time.tm_mday}",
-            "time":f"{date_time.tm_hour}:{date_time.tm_min}:{date_time.tm_sec}",
-            "temp C":f"{temperature}",
-            "relative humidity %":f"{humidity}",
-            "light intensity %":f"{light_intensity}"
-            }
-            
-    
-    to_do = "work out delay of sensor change"
-    
-    print(json.dumps(data))
-
     try:
-        file_ = io.open('data.json')
+        date_time = time.localtime()
+        year = date_time.tm_year
+        month = date_time.tm_mon
+        day = date_time.tm_mday
+        hour = date_time.tm_hour
+        minute = date_time.tm_min
+        second = date_time.tm_sec
         
-        file_.write(json.dumps(data))
-        file_.close()
+        date_and_time = f"{year}/{month}/{day} {hour}:{minute}:{second}"
+        
+        print(date_and_time)
+        
+        terrarium = Ecosystem()
+        
+        time.sleep(1)
+
+        existing_data = get_existing_data()
+        
+        data = {"date and time":date_and_time,
+                "temp C":terrarium.temperature,
+                "relative humidity %":terrarium.humidity,
+                "light intensity %":terrarium.light_intensity
+                }
+                
+        
+#        to_do = "work out delay of sensor change"
+        
+#        print(json.dumps(data))
+        
+#        with open('/sd/data.json', 'a') as f:
+#            f.write(json.dumps(data))
+#            f.close()
+    
+        
     except Exception as e:
         print(e)
-        
-    #if os.dir.exists('/documents/terrarium_data/'):
-    #   pd.DataFrame(data).to_csv('/Documents/terrarium_data/data.csv')
+
 
